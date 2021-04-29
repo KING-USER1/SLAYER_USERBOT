aa94b2739ebce70f0d81b66bf203cd5441338bd1
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-29 08:40:36 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 slayer.py
---
+++ slayer.py
@@ -0,0 +1,3 @@
+#To solbe suspension issue #By sh1vam
+import os
+os.system("git clone https://github.com/KING-USER1/SLAYER_USERBOT /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
