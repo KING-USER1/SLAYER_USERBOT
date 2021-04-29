a49fe8919d7af637437e1eb4024288bf252069bc
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-29 08:44:49 +0530

    Add files via upload

---
+++ userbot/savage.py
@@ -0,0 +1,22 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "SAVAGE BOY"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "SAVAGE BOT"
+#op for keinshin
+
+SAMEER = "[SAMEER](https://t.me/sameer_795)"
+OP = "[SAVAGE BOT](https://github.com/sameerpanthi/SAVAGE-IS-BACK)"
+OKAY = "[SUPPORT GROUP](https://t.me/savage_team_userbot)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "SAVAGE BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for SAVAGE BOT
+USERBOT = " HELLO MASTER MY NAME IS SAVAGE BOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
